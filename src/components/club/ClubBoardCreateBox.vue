88276e910797084621fce647c61f1b2ee93d1444
030ii <lovelygeee2@gmail.com>
2020-09-20 13:53:34 +0900

    #39 모임 게시판 화면 구성

---
+++ src/components/club/ClubBoardCreateBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="field-wrapper">
+        <v-text-field class="pa-0"
+                        label="글 제목"
+        />
+        <v-textarea outlined
+                    label="내용을 작성해주세요."
+        ></v-textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ClubBoardCreateBox',
+    methods: {
+        
+    },
+};
+</script>
+
+<style scoped>
+.field-wrapper {
+    margin-top: 2rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+}
+</style>
---
+++ src/components/common/CommonComments.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- 댓글 영역 -->
+        <!-- 하단고정 댓글입력 -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CommonComments',
+    // props: , // 댓글 seq
+};
+
+</script>
+
+<style scoped>
+
+</style>
