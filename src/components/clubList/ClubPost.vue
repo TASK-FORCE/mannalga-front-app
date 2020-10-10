39caa35cc88d68459538f9932c4cce15b2ff8a5b
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2020-10-10 23:36:15 +0900

    ClubPost 수정

--- src/components/clubList/ClubPost.vue
+++ src/components/clubList/ClubPost.vue
@@ -2,45 +2,37 @@
     <div>
         <v-list-item :key="club.seq"
                      :to="`/club/${club.seq}`"
+                     class="pa-3"
         >
-            <div>
-                <v-img :src="imgUrl"
-                       max-width="60"
-                       max-height="50"
-                />
-            </div>
-            <div class="ml-5 w-100">
-                <v-row>
-                    <v-col cols="10">
-                        <div>
-                            <v-btn fab
-                                   width="24"
-                                   height="24"
-                                   outlined
-                                   :color="interestGroupType.color"
-                            >
-                                <v-icon class="interest-icon">{{ interestGroupType.icon }}</v-icon>
-                            </v-btn>
-                            <span class="ml-1"
-                                  style="font-size: 0.9rem"
-                            >
+            <v-img :src="imgUrl"
+                   max-width="60"
+                   max-height="50"
+            />
+            <div class="ml-5">
+                <div>
+                    <v-btn fab
+                           width="24"
+                           height="24"
+                           outlined
+                           :color="interestGroupType.color"
+                    >
+                        <v-icon class="interest-icon">{{ interestGroupType.icon }}</v-icon>
+                    </v-btn>
+                    <span class="ml-1"
+                          style="font-size: 0.9rem"
+                    >
                                 {{ firstRegion.name }}
                             </span>
-                        </div>
-                        <div class="mt-1">
-                            {{ club.id }} {{ club.name }}
-                        </div>
-                    </v-col>
-                    <v-col align-self="center"
-                           class="pa-0 text-body-2"
-
-                    >
-                        <div style="font-size: 0.8rem">
-                            {{ club.userCount }}/{{ club.maximumNumber }}
-                        </div>
-                    </v-col>
-                </v-row>
-
+                </div>
+                <div class="mt-1">
+                    {{ club.name }}
+                </div>
+            </div>
+            <v-spacer />
+            <div class="text-body-2 mr-3">
+                <div style="font-size: 0.8rem">
+                    {{ club.userCount }}/{{ club.maximumNumber }}
+                </div>
             </div>
         </v-list-item>
         <v-divider />
