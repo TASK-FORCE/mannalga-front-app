79cca28dc75f8e6e1e6e1e09f5a654fb800df837
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2020-11-29 14:26:40 +0900

    MeetingList api 추가

--- src/apis/MeetingApi.js
+++ src/apis/MeetingApi.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
+import ResponseConverter from '@/apis/converter/ResponseConverter.js';
 
 class MeetingApi {
     postClubMeetingCreate({ clubSeq, clubMeetingCreateDto }) {
         return axios.post(`/api/clubs/${clubSeq}/meetings`, clubMeetingCreateDto);
+    }
+
+    getMeetingList({ clubSeq, requestParams }) {
+        return axios.get(`/api/clubs/${clubSeq}/meetings`, { params: requestParams })
+            .then(ResponseConverter.extractSuperInventionResponseData);
     }
 }
 
