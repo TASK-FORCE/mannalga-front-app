15444e61765ce519b0b11c2f2ad548e10eb3b0c2
Dongmyeong Lee <dongmyeong.lee22@gmail.com>
2021-01-02 23:41:26 +0900

    댓글 작성 후 댓글 조회 최적화

--- src/store/modules/album.js
+++ src/store/modules/album.js
@@ -49,6 +49,17 @@ const mutations = {
         state.albumCommentPage = albumCommentPage;
     },
 
+    addNextAlbumCommentListWithCheckDuplicate(state, { albumCommentList, albumCommentPage }) {
+        albumCommentList.forEach(comment => {
+            // TODO: seq를 내려주면 그거로 변경하자.
+            const isPresentInList = state.albumCommentList.find(({ registerTime, content }) => (registerTime === comment.registerTime) && (content === comment.content));
+            if (!isPresentInList) {
+                state.albumCommentList.push(comment);
+            }
+        });
+        state.albumCommentPage = albumCommentPage;
+    },
+
     initAlbumCommentList(state) {
         state.albumCommentList = [];
         state.albumCommentPage = defaultBuilder.buildPage();
@@ -115,6 +126,18 @@ const actions = {
     },
 
     async requestAllAlbumCommentListWithPaging({ commit, state }, albumCommentRequestInfo) {
+        // 마지막 페이지면 현재 페이지를 조회하여 refresh해준다.
+        if (state.albumCommentPage.isLastPage) {
+            const requestDto = {
+                ...albumCommentRequestInfo,
+                requestParams: {
+                    size: state.albumCommentPage.size,
+                    page: state.albumCommentPage.currentPage,
+                },
+            };
+            const albumListInfo = await albumApi.getClubAlbumCommentList(requestDto);
+            commit('addNextAlbumCommentListWithCheckDuplicate', albumListInfo);
+        }
         const requestCommentListRecursive = async () => {
             if (state.albumCommentPage.isLastPage) return;
 
