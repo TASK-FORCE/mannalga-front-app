61a87b95662e2c940997c6d0825a9da8ed0fc8d7
dongmyeonglee <dongmyeong.lee22@gmail.com>
2021-04-19 22:10:39 +0900

    theme 변경 시 body tag에도 적용

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -19,10 +19,17 @@ const changeTheme = (): void => {
   isDarkTheme() ? localStorage.setItem(KEY, Theme.WHITE) : localStorage.setItem(KEY, Theme.DARK);
 };
 
+
+const setBodyTagStyle = (isDarkTheme: boolean) => {
+  const bodyElement = document.querySelector('body') as HTMLBodyElement;
+  bodyElement.style.backgroundColor = isDarkTheme ? '#121212' : '#FFFFFF';
+};
+
 export const isDarkTheme = (): boolean => currentTheme() !== Theme.WHITE;
 
 export const loadCurrentTheme = (): void => {
   vuetify.framework.theme.dark = isDarkTheme();
+  setBodyTagStyle(isDarkTheme());
 }
 
 export const changeThemeAndLoad = (): void => {
